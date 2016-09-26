20be2f78a8cb5b920b153782914c43ef8db08bb9
keshavmishra06 <Keshav.mishra48@gmail.com>
2016-09-26 21:49:01 +0530

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,46 @@
+import msvcrt
+import time
+
+f1=10
+f2=20
+f3=30
+
+count=0
+count1=10
+count2=20
+print"press enter key to proceed!!"
+
+raw_input()
+s_time=time.time()
+
+while(1):
+		key=msvcrt.getch()
+		if key=='d':
+					count=count+1
+					print"-->",
+					if count==f1:
+								print"turn down"
+								break
+while(1):
+		key1=msvcrt.getch()
+		if key1=='s':
+						count1=count1+1
+						print"                                   "
+						print"                                      |"
+						if count1==f2:
+									print"                            turn right"
+									print"                                       ",
+									break
+while(1):
+		key2=msvcrt.getch()
+		if key2=='d':
+
+						count2=count2+1
+						print "-->",
+
+						if count2==f3:
+									break
+time_elapsed=time.time()-s_time
+print"u have completed the game"
+print"the time taken is"+str(time_elapsed)
+
